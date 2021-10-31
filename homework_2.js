329ffd99f2bc13407f4bee7d3b5327f3d25ef008
Vladimir Danilov <evazyled@gmail.com>
2021-10-31 14:29:39 +0300

    homework_2

---
+++ homework_2.js
@@ -0,0 +1,17 @@
+"кот" > "код" // кот
+"2" + 2 * "2" // 24 
+ undefined == null // true
+ undefined === null //false 
+null == 0  // false 
+ 2 > "3" // false
+ null - false + true // 1
+ 1 / "l" // 1
+ "2" * "3" // 6
+ 4 + 5 + "O" // 9
+"l" + 4 + 5 // 145
+ "4" - 2 //2
+ "4" - "4x" // NaN
+null == false // false
+" -4 "/ 0 + 1 // infinity 
+ null + 1 // 1
+undefined + null // NaN
